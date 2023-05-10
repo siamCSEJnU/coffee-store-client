d3f9886f8570a0c2d52c3191a97d7532cafcc220
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-05-11 00:21:08 +0600

    a form for adding coffee to the server through database and fetching with post method implemented

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,151 @@
+import React from "react";
+
+const AddCoffee = () => {
+  const handleAddCoffee = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const quantity = form.quantity.value;
+    const details = form.details.value;
+    const category = form.category.value;
+    const photo = form.photo.value;
+
+    const newCoffee = {
+      name,
+      supplier,
+      taste,
+      quantity,
+      details,
+      category,
+      photo,
+    };
+    console.log(newCoffee);
+
+    //send data to the server
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  return (
+    <div className="bg-[#f4f3f0] p-24">
+      <h2 className="text-3xl font-extrabold">Add a new coffee</h2>
+      <form onSubmit={handleAddCoffee}>
+        {/* name and quantity row */}
+        <div className="md:flex mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Coffee Name"
+                name="name"
+                className="input input-bordered w-full" // medium device er jonno half space nibe tar moddhe input nibe full space
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 ml-4">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="quantity"
+                placeholder="Available Quantity"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        {/* supplier and taste row */}
+        <div className="md:flex mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Supplier Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Supplier Name"
+                name="supplier"
+                className="input input-bordered w-full" // medium device er jonno half space nibe tar moddhe input nibe full space
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 ml-4">
+            <label className="label">
+              <span className="label-text">Taste</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="taste"
+                placeholder="Taste"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        {/* Category and Details row */}
+        <div className="md:flex mb-8">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Category</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Category"
+                name="category"
+                className="input input-bordered w-full" // medium device er jonno half space nibe tar moddhe input nibe full space
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 ml-4">
+            <label className="label">
+              <span className="label-text">Details</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="details"
+                placeholder="Details"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        {/* form photo url row */}
+        <div className="mb-8">
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Photo URL"
+                name="photo"
+                className="input input-bordered w-full" // medium device er jonno half space nibe tar moddhe input nibe full space
+              />
+            </label>
+          </div>
+        </div>
+        <input type="submit" value="Add Coffee" className="btn btn-block" />
+      </form>
+    </div>
+  );
+};
+
+export default AddCoffee;
